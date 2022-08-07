d5e078665b5f99897c3c53c4b1c5e1dfea7f1df2
Reinaldo Martins de Padua <reinaldo.padua@lojasrenner.com.br>
2022-08-07 15:43:24 -0300

    inicializando repositório

---
+++ src/main/java/edu/br/unoesc/ulife/login/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package edu.br.unoesc.ulife.login.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(Model model) {
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/edu/br/unoesc/ulife/login/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package edu.br.unoesc.ulife.login.repositories;
+
+import java.util.Optional;
+
+import edu.br.unoesc.ulife.login.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>  {
+    public Optional<User> findByUsername(String username);
+}
